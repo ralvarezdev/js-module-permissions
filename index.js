c331fdc8a54a98fcb0bd9de84afebc7f7dbca123
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-27 10:23:24 -0400

    refactor: modified main index.js

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-export {default as ModuleManager} from "./permissions/module.js";
-export {default as Route} from "./permissions/route.js";
-export {default as MethodManager} from "./permissions/method.js";
-export {default as ObjectManager} from "./permissions/object.js";
+export {ModuleManager, Route, MethodManager, ObjectManager} from "./permissions";
